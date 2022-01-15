7e307ebfe0b326280fa22e406b87836e4b688918
souzajackson <73433159+souzajackson@users.noreply.github.com>
2022-01-15 14:46:38 -0300

    Initial commit

---
+++ calculadora-de-equacoes.py
@@ -0,0 +1,113 @@
+from funcoes import *
+class Equacao(object):
+    """
+    Classe que armazena uma equação polinomial de qualquer grau.
+    """
+    def __init__(self, coeficientes):
+        """
+        Recebe os coeficientes da equação (em ordem crescente).
+        """
+        self.coeficientes = coeficientes
+
+    
+    def __repr__(self) -> str:
+        """
+        Retorna uma representação da equação. 
+        """
+        expressao = ''
+
+        for pos, coeficiente in enumerate(self.coeficientes):
+            e = len(self.coeficientes) - pos - 1
+            expressao += f'({coeficiente}) * (x)**{e} + '
+        expressao  = expressao.replace(' * (x)**0 + ', ' = 0')
+
+        return expressao
+
+    def e_raiz(self, raiz):
+        """
+        Calcula se um valor pode ser raiz da equação.
+        """
+        return eval(self.__repr__().replace(' = 0', ' == 0').replace('x', str(raiz)))
+
+
+    def possiveisRaizesReais(self):
+        """
+        Retorna uma lista com os valores reais candidatos a serem uma raiz da equação.
+        """
+        termoMaiorGrau = int(self.coeficientes[0])
+        termoIndependente = int(self.coeficientes[-1])
+        divisoresTermoIndependente = list()
+        divisoresTermoMaiorGrau = list()
+        for c in range(1, abs(termoIndependente) + 1):
+            if termoIndependente % c == 0:
+                divisoresTermoIndependente.append(c)
+                divisoresTermoIndependente.append(-c)
+        for c in range(1, abs(termoMaiorGrau) + 1):
+            if termoIndependente % c == 0:
+                divisoresTermoMaiorGrau.append(c)
+                divisoresTermoMaiorGrau.append(-c)
+
+        possiveisRaizes = list()
+        for numerador in divisoresTermoIndependente:
+            for denominador in divisoresTermoMaiorGrau:
+                possiveisRaizes.append(f'{numerador}/{denominador}')
+        possiveisRaizes.append('0')
+
+        return possiveisRaizes
+
+    
+    def raizes(self):
+        """
+        Calcula as raízes da equação e as retorna em uma lista.
+        """
+        testes = self.possiveisRaizesReais()
+        expressao = self
+        coeficientes = self.coeficientes
+        raizes = list()
+        while len(coeficientes) > 3:
+            for teste in testes:
+                if self.e_raiz(teste):
+                    raizes.append(teste)
+                    novosCoeficientes = briotRuffinne(coeficientes, teste)
+                    expressao = Equacao(novosCoeficientes)
+                    coeficientes = expressao.coeficientes
+                    if teste == '0':
+                        testes = expressao.possiveisRaizesReais()
+                    break
+            else:
+                break
+
+        if len(coeficientes) == 3:
+            raizesRestantes = bhaskara(expressao.__repr__().replace(' = 0', ''))
+            raizes.append(raizesRestantes[0])
+            raizes.append(raizesRestantes[1])
+        elif len(coeficientes) == 2:
+            raizes.append(f'{-coeficientes[1]}/{coeficientes[0]}')
+
+        return simplificado(raizes)
+
+
+while True:
+    try:
+        coeficientes = list()
+        grauDaEquacao = int(input('Qual o grau da equação? '))
+        for c in range(grauDaEquacao):
+            e = grauDaEquacao - c
+            coeficiente = float(input(f'Digite o coeficiente do x**{e}: '))
+            coeficientes.append(coeficiente)
+        termoIndependente = input('Digite o termo independente: ')
+        coeficientes.append(termoIndependente)
+
+        equacao = Equacao(coeficientes)
+        raizes = equacao.raizes()
+        
+        print('S = {', end='')
+        for pos, raiz in enumerate(raizes):
+            if pos == len(raizes) - 1:
+                print(raiz, end='}\n')
+            else:
+                print(raiz, end=', ')
+
+    except KeyboardInterrupt:
+        print('\nPrograma finalizado!')
+        break
---
+++ funcoes.py
@@ -0,0 +1,75 @@
+def simplificado(fracoes):
+    """
+    Parâmetro: Uma fração (em formato de string) 
+    Return: Uma fração irredutível ou um número inteiro, ambos em formato de string.
+    """
+    simplificados = list()
+    for fracao in fracoes:
+        if 'sqrt' not in fracao and '/' in fracao:
+            raiz = fracao.split('/')
+            numerador = int(float(raiz[0]))
+            denominador = int(float(raiz[1]))
+            divisoesFeitas = 1
+            while divisoesFeitas != 0:
+                divisoesFeitas = 0
+                for c in range(2, denominador + 1):
+                    if denominador % c == 0 and numerador % c == 0:
+                        numerador //= c
+                        denominador //= c
+                        divisoesFeitas += 1
+            if denominador == 1:
+                simplificados.append(str(numerador))
+            elif denominador == -1:
+                simplificados.append(str(-numerador))
+            else:
+                simplificados.append(f'{numerador}/{denominador}')
+        else:
+            simplificados.append(fracao)
+    return simplificados
+
+
+def bhaskara(expressao):
+        """
+        Parâmetro: Expressão contendo uma equação do segundo grau.
+        Return: Uma tupla contendo duas raízes da equação.
+        """
+        from math import sqrt
+        expressaoEmLista = list(expressao)
+        coeficientes = list()
+        for pos, char in enumerate(expressaoEmLista):
+            if char == '(':
+                novoCoficiente = ''
+                posAtual = pos + 1
+                while expressaoEmLista[posAtual] != ')':
+                    novoCoficiente += expressaoEmLista[posAtual]
+                    posAtual += 1 
+                coeficientes.append(novoCoficiente)
+        while 'x' in coeficientes:
+            coeficientes.remove('x')
+        a = float(coeficientes[0])
+        b = float(coeficientes[1])
+        c = float(coeficientes[2])
+        delta = (b) ** 2 - 4 * a * c
+        if delta >= 0 and int(sqrt(delta)) == sqrt(delta):
+            x1 = f'{(-b + sqrt(delta))}/{2 * a}'
+            x2 = f'{(-b - sqrt(delta))}/{2 * a}'
+        else:
+            x1 = f'(-{b} + sqrt({delta})) / {2 * a}'.replace('--', '+')
+            x2 = f'(-{b} - sqrt({delta})) / {2 * a}'.replace('--', '+')
+        return x1, x2
+
+
+def briotRuffinne(coeficientes, raiz):
+    """
+    Parâmetro1: Uma lista com os coeficientes da equação.
+    Parâmetro2: Uma raiz da equação.
+    Return: Uma lista com os coeficientes da equação reduzida em um grau.
+    """
+    novosCoeficientes = list()
+    for coeficiente in coeficientes:
+        if len(novosCoeficientes) == 0:
+            novosCoeficientes.append(coeficiente)
+        else:
+            novosCoeficientes.append(novosCoeficientes[-1] * eval(str(raiz)) + int(coeficiente))
+    novosCoeficientes.pop()
+    return novosCoeficientes
